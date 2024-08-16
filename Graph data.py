83bf45b8e29e0d94586ef9762670503e6b9fdc71
Dbot453 <dhzheng@perse.co.uk>
2024-08-16 16:21:47 +0100

    removed lrt from distances

--- Graph data.py
+++ Graph data.py
@@ -22,22 +22,49 @@ class graph:
                         result.append(fields[i])
                     adj_stations = fields[len(fields)-1].split("#")
                     result.append(adj_stations)
-                    
+                    result.append({})
                     stations[fields[0]] = result
-
+                    
+        station_codes = stations.keys()
+        distance_station_codes = []
+        
         with open("distances.csv", 'r') as distances_file:
             for line in distances_file:
                 line = line.strip()
                 fields = line.split(",")      
-                result = []
+                
+                distance_station_codes.append(fields[0])
+                distance_station_codes.append(fields[1])
                 
                 if fields[0] != "Station1":
-                    temp = f"{fields[0]} {fields[1]}"
-                    distances[temp] = fields[3]
+                    station_value = stations[fields[0]]#station 1 in distances.csv
+                    set = station_value[6]#copies the set from the station_value
+                    set[fields[1]] = fields[3]#adds the distance from station 1 to station 2 to the set
+                    station_value[6] = set #copies the set back to the station_value
+                    stations[fields[0]] = station_value
+                    
+                    station_value = stations[fields[1]]#station 2 in distances.csv
+                    set = station_value[6]
+                    set[fields[0]] = fields[3]
+                    station_value[6] = set
+                    stations[fields[1]] = station_value
+                
+                    # station_value = stations[fields[1]]
+                    # adj_stations = station_value[5]
+                    # temp[fields[0]] = fields[3]
+                    # station_value.append(temp)
+                    # stations[fields[1]] = station_value
             
-        print(distances)      
+        # print(stations)
+        for k in stations:
+            if len(stations[k][6]) != len(stations[k][5]):
+                for i in stations[k][5]:
+                    if i not in stations[k][6]:
+                        print(f"{k} to {i} is missing in distances.csv")
+    
+               
         visited = []
-        distances = []                                        
+        distances = []
         for keys in stations:
             if keys == start:
                 visited.append(True)
@@ -45,8 +72,16 @@ class graph:
             else:
                 visited.append(False)
                 distances.append(math.inf)
-        
+                
         current = start
+        # while current != end:
+        #     for i in range(0,len(stations[current])):
+        #         if visited[i] == False:
+        #             if distances[i] > distances[current]+distances[f"{current} {stations[current][2][i]}"]:
+        #                 distances[i] = distances[current]+distances[f"{current} {stations[current][2][i]}"]
+        #     visited[current] = True
+        #     current = distances.index(min(distances))
+        
        
     
-graph().djikstra("Jurong East","Bayfront")
+graph().djikstra("NS2","CC18")
